f40d47a0c1558811357e548f4506b891d19edba5
Daniel Arango Hoyos <d.arango@globant.com>
2023-08-24 11:32:29 -0500

    Part Time Teachers implemented with tests

---
+++ src/actors/PartTimeTeacher.java
@@ -0,0 +1,21 @@
+package actors;
+
+public class PartTimeTeacher extends Teacher {
+    double activeWeeklyHours;
+
+    PartTimeTeacher(String name, int age, int id, double baseSalary, double activeWeeklyHours){
+        super(name, age, id, baseSalary);
+        this.activeWeeklyHours = activeWeeklyHours;
+        this.salary = calculateSalary();
+    }
+
+    @Override
+    public double calculateSalary(){
+        return baseSalary * activeWeeklyHours;
+    }
+
+    @Override
+    public String toString(){
+        return formatting() + "\tWeekly Hours: " + activeWeeklyHours;
+    }
+}
