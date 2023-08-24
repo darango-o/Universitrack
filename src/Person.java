e661da4f8e4a0b9ca2ae2da2ca87848cb84fd1e7
Daniel Arango Hoyos <d.arango@globant.com>
2023-08-24 09:45:42 -0500

    Added abstract class Person

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public abstract class Person {
+    protected String name, type;
+    protected int age, id;
+
+    Person(String name, int age, int id, String type){
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.type = type;
+    }
+
+    public abstract String toString();
+
+    public String formatting(){
+      return String.format("\nType: %s\tId: %d\tName: %s\tAge: %d years old", type, id, name, age);
+    };
+}
